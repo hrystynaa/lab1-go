7360dfe09ae3194041ce05477688cd2fd588a4c2
hrystynaa <yaskulskahrystyna@gmail.com>
2023-02-28 11:50:29 +0200

    Add error handling

--- server.go
+++ server.go
@@ -22,6 +22,10 @@ func main() {
 }
 
 func realTimeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Error: you can use only GET method", http.StatusNotFound)
+		return
+	}
 	realTime := time.Now().Format(time.RFC3339)
 	response := map[string]string{"time": realTime}
 	w.Header().Set("Content-Type", "application/json")
